601dd66fd404924d7bde1972173607846730478f
DominikGos <dominikgos3@gmail.com>
2025-03-08 18:12:32 +0100

    Added data initialization

---
+++ book-storage/src/main/java/com/microservices/library/entities/Book.java
@@ -0,0 +1,50 @@
+package com.microservices.library.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    public Book() {}
+
+    public Book(String title, int id, String author) {
+        this.title = title;
+        this.id = id;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ book-storage/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO books (title,  author)
+VALUES ('Harry Potter and the Order of the Phoenix', 'J.K. Rowling'),
+       ('The Great Gatsby', 'F. Scott Fitzgerald'),
+       ('Crime and Punishment', 'Fyodor Dostoevsky'),
+       ('The da Vinci Code', 'Dan Brown');
