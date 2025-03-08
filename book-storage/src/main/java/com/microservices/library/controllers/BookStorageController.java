f801a71ad349b878e68ab5d1e365eb9b163155b2
DominikGos <dominikgos3@gmail.com>
2025-03-08 18:25:14 +0100

    Added books listing

--- book-storage/src/main/java/com/microservices/library/controllers/BookStorageController.java
+++ book-storage/src/main/java/com/microservices/library/controllers/BookStorageController.java
@@ -1,16 +1,28 @@
 package com.microservices.library.controllers;
 
+import com.microservices.library.entities.Book;
+import com.microservices.library.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/books")
 public class BookStorageController {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookStorageController(final BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @GetMapping("")
-    public String index() {
-        return "Hello World";
+    public List<Book> index() {
+        List<Book> books = bookRepository.findAll();
+        return books;
     }
 }
