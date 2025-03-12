689c13de9657bff6ac4648e4777681c41d068381
DominikGos <dominikgos3@gmail.com>
2025-03-12 11:42:43 +0100

    Added naming-server

--- book-borrowing/src/main/java/com/microservices/library/bookborrowing/controllers/BookStorageProxy.java
+++ book-borrowing/src/main/java/com/microservices/library/bookborrowing/controllers/BookStorageProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="book-storage", url="localhost:8100")
+@FeignClient(name="book-storage")
 public interface BookStorageProxy {
     @DeleteMapping("/books/{id}")
     Book deleteBook(@PathVariable("id") long id);
